a983bdf746ce899c96c42cddbbf7757c56ad2dde
Raffi Maulana Hadi <raffimaulana25@gmail.com>
2024-05-19 20:33:42 +0700

    updated edit product

--- util/database.js
+++ util/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("db_node-complete", "root", "", {
   dialect: "mysql",
   host: "localhost",
+  timezone: "+07:00",
 });
 
 module.exports = sequelize;
