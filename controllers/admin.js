620a8e833f3c9fbaa3b95830e738576beb909f9c
Raffi Maulana Hadi <raffimaulana25@gmail.com>
2024-05-20 23:51:28 +0700

    update controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -33,7 +33,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findById(prodId)
+  Product.findByPk(prodId)
     .then(product => {
       if (!product) {
         return res.redirect("/");
