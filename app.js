50a802fbc71cb8f4afd6937a29b4a1071f1998e0
Raffi Maulana Hadi <raffimaulana25@gmail.com>
2024-05-29 19:50:50 +0700

    chore: Add User model and establish association with Product model

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -23,8 +25,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+
 sequelize
-  .sync()
+  .sync({ force: true })
   .then(result => {
     // console.log(result);
     app.listen(3000);
